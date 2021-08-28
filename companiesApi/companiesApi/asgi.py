a3825f37cc05df255777d5869766b9c041f98399
Emanuel <emanuelX92@gmail.com>
2021-08-28 00:52:31 -0400

    initial backend ok

--- companiesApi/companiesApi/asgi.py
+++ companiesApi/companiesApi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for copaniesApi project.
+ASGI config for companiesApi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'copaniesApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'companiesApi.settings.local')
 
 application = get_asgi_application()
